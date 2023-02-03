dc4a02d3aaea74fac5a60deafb30f6b1f6f4f76d
donghune <reona7140@naver.com>
2023-02-03 18:44:11 +0900

    feature: 파이어베이스에 유저 등록 및 인증 로직 추가

---
+++ core/data/src/main/java/io/github/loacq/data/auth/AuthRepository.kt
@@ -0,0 +1,9 @@
+package io.github.loacq.data.auth
+
+import com.google.firebase.auth.FirebaseUser
+
+
+interface AuthRepository {
+    suspend fun getCurrentUser() : FirebaseUser?
+    suspend fun addUserToFirestore()
+}
